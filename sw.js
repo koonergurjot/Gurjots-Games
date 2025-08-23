e111045122a9fe1d0a8de371edca229d807b6e2b
koonergurjot <xxgurjotxx@gmail.com>
2025-08-22 19:14:58 -0700

    feat: cache navigated pages

--- sw.js
+++ sw.js
@@ -1,2 +1,40 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
+const CACHE = 'static';
+const ASSETS = [
+  'index.html',
+  'main.js',
+  'styles.css',
+  'games/box3d/index.html',
+  'games/pong/index.html',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE);
+      await cache.addAll(ASSETS);
+      await self.skipWaiting();
+    })(),
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      (async () => {
+        const cache = await caches.open(CACHE);
+        let path = new URL(e.request.url).pathname;
+        if (path.endsWith('/')) {
+          path += 'index.html';
+        }
+        path = path.replace(/^\//, '');
+        const cached = await cache.match(path);
+        return cached || fetch(e.request);
+      })(),
+    );
+  }
+});
+
