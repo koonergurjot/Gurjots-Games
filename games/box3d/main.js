29827caead6dc3f615ceca90fd9d9a427eb16126
Gurjot Kooner <226573228+koonergurjot@users.noreply.github.com>
2025-09-16 15:40:40 -0700

    Fix OrbitControls import for box3d

--- games/box3d/main.js
+++ games/box3d/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js?module';
 import { injectBackButton, recordLastPlayed, saveBestScore } from '../../shared/ui.js';
 
 const GAME_ID = 'box3d';
