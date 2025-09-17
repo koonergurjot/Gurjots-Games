28340b78c51ae9771ece04a5ebe1d123b01158b4
Gurjot Kooner <226573228+koonergurjot@users.noreply.github.com>
2025-09-17 01:17:33 -0700

    Align Maze3D three.js modules

--- games/maze3d/main.js
+++ games/maze3d/main.js
@@ -1,4 +1,4 @@
-import { PointerLockControls } from "https://unpkg.com/three@0.161.0/examples/jsm/controls/PointerLockControls.js";
+import { PointerLockControls } from "./PointerLockControls.js";
 import '/js/three-global-shim.js';
 import { injectHelpButton, recordLastPlayed, shareScore } from '../../shared/ui.js';
 import { emitEvent } from '../../shared/achievements.js';
